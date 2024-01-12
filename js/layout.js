81f849cfc7bc1b465e89bca83f7a06022bcfa377
unknown <alex@naver.com>
2024-01-12 19:42:02 +0900

    240112 작업

--- js/layout.js
+++ js/layout.js
@@ -1,22 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     // gnb
-    const depth01Links = document.querySelectorAll('.depth01-link');
+    const depth01Links = document.querySelectorAll('.depth01 > li > a');
 
     depth01Links.forEach((depth01Link) => {
         const depth02s = document.querySelectorAll('.depth02');
-
-        depth01Link.addEventListener('mouseenter', function () {
-            depth01Links.forEach((e) => {
-                e.style.color = '#888';
-            });
-            depth01Link.style.color = '#000';
-        });
-
-        depth01Link.addEventListener('mouseleave', function () {
-            depth01Links.forEach((e) => {
-                e.style.color = '#000';
-            });
-        });
 
         depth01Link.addEventListener('click', function () {
             if (depth01Link.nextElementSibling.classList.contains('active')) {
@@ -28,5 +15,41 @@ document.addEventListener('DOMContentLoaded', () => {
                 depth01Link.nextElementSibling.classList.add('active');
             }
         });
+    });
+
+    document.addEventListener('mouseup', (e) => {
+        let depth02Active = document.querySelector('.depth02.active');
+
+        if (depth02Active !== null) {
+            if (!depth02Active.contains(e.target)) {
+                depth02Active.classList.remove('active');
+            }
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        let currentScrollY = window.pageYOffset;
+        const header = document.querySelector('header');
+        let headerAct = true;
+        let currentAct = false;
+
+        currentScrollY > header.offsetTop ? (currentAct = true) : (currentAct = false);
+        console.log(headerAct, '1');
+        if (headerAct === currentAct) {
+            if (currentAct) {
+                header.classList.add('border');
+                console.log('add');
+                console.log(headerAct, '2');
+            } else {
+                header.classList.remove('border');
+                console.log('remove');
+                headerAct = true;
+                console.log(headerAct);
+            }
+            console.log(headerAct, '3');
+            headerAct = false;
+        }
+        console.log(headerAct, '4');
+        console.log(currentAct, '5');
     });
 });
